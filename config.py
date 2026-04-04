758ecfbeea02b26dada611c48f86864ae35e4345
SayedHanan <hananhaider.pk@gmail.com>
2026-04-04 19:00:06 +0500

    Added the embedder.py and its related EmbeddingConfig: in config.py

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class IngestionConfig:
     def __init__(self, chunk_size: int = 1000, chunk_overlap: int = 200):
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
+
+
+class EmbeddingConfig:
+    def __init__(self, model_name: str, api_key: str, environment: str = None):
+        self.model_name = model_name
+        self.api_key = api_key
+        self.environment = environment
--- main.py
+++ main.py
@@ -14,14 +14,28 @@
 
 
 
+# from ingestion.loader import DataLoader
+# from ingestion.chunker import Chunker
+# from config import IngestionConfig
+
+# if __name__ == "__main__":
+#     data_loader = DataLoader("./sample.json")
+#     documents = data_loader.load()
+#     chunker = Chunker(IngestionConfig())
+#     chunks = chunker.chunk(documents)
+#     print(chunks)   
+
+from config import EmbeddingConfig, IngestionConfig
+from ingestion.embedder import Embedder
 from ingestion.loader import DataLoader
 from ingestion.chunker import Chunker
-from config import IngestionConfig
 
 if __name__ == "__main__":
-    data_loader = DataLoader("./sample.json")
-    documents = data_loader.load()
+    loader = DataLoader("./sample.json")
+    documents = loader.load()
     chunker = Chunker(IngestionConfig())
-    print("These are the chunks")
     chunks = chunker.chunk(documents)
-    print(chunks)   
+    embedding_config = EmbeddingConfig(model_name="llama-text-embed-v2", api_key="pcsk_37VQ9v_PE1gFCN6aFXo5WfcNjHK1Pf1QWm7VEn5hpCAcuNb76AG2g9TdCpv8B3GMFh83Da")
+    embedder = Embedder(embedding_config)
+    query_embedding = embedder.embed_documents(chunks)
+    print(query_embedding)
