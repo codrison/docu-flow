8e7fb141476babdd09a57449b791bc3b2a87c30b
SayedHanan <hananhaider.pk@gmail.com>
2026-04-08 19:02:49 +0500

    refactored: Rafactor into Object Oriented Paradigm

--- prompts/rag.py
+++ prompts/rag.py
@@ -1,9 +1,12 @@
-def build_messages(system_prompt, history, query, relevant_docs):
-    context = "\n\n".join([doc.page_content for doc in relevant_docs])
-    prompt = f"Context:\n{context}\n\nQuestion: {query}"
-    
-    return [
-        {"role": "system", "content": system_prompt},
-        *history,
-        {"role": "user", "content": prompt}
-    ]
+from langchain_core.documents import Document
+
+class RAG:
+    def build_messages(self, relevant_docs, query, history):
+        context = "\n\n".join([doc.page_content for doc in relevant_docs])
+        prompt = f"Context: \n{context}\n\n Question: {query}"
+
+        return [
+            {"role": "system", "content": "system_prompt"},
+            *history,
+            {"role": "user", "content": prompt}
+        ]
